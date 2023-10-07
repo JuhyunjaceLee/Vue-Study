5d27fee066680d32cf9253adfa67c331f0172c26
Lee Ju-Hyun <heyjace777@gmail.com>
2023-10-07 19:09:07 +0900

    SFC and props

---
+++ src/components/AppCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <!-- {{ $props }} -->
+      <!-- type: news, notice -->
+      <span class="badge bg-secondary">{{ badgeName }}</span>
+      <h5 class="card-title red mt-2">{{ title }}</h5>
+      <p class="card-text" :class="$style.red">
+        {{ contents }}
+      </p>
+      <a href="#" class="btn" :class="isLikeClass" @click="toggleLike()"
+        >좋아요</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+console.log("AppCard Module");
+import { computed } from "vue";
+
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    contents: {
+      type: String,
+      required: true,
+    },
+    badge: {
+      type: String,
+      default: "NEWS",
+      // validator: (value) => {
+      //   return ["NEWS", "NOTICE"].includes(value);
+      // },
+    },
+    isLike: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ["toggleLike"],
+  setup(props, context) {
+    console.log("AppCard Setup");
+    //AppCard 인스턴스가 6번 생성되었기 때문에 콘솔도 6번 찍힘.
+    const badgeName = computed(() =>
+      props.badge === "NEWS" ? "NEWS" : "NOTICE"
+    );
+    const isLikeClass = computed(() =>
+      props.isLike ? "btn-danger" : "btn-outline-danger"
+    );
+    const toggleLike = () => {
+      context.emit("toggleLike");
+    };
+    return { isLikeClass, badgeName, toggleLike };
+  },
+};
+</script>
+
+<style module>
+.red {
+  color: grey !important;
+}
+</style>
---
+++ src/components/TheView.vue
@@ -0,0 +1,89 @@
+<template>
+  <main>
+    <div class="container py-4">
+      <div class="row g-3">
+        <div v-for="post in posts" :key="post.id" class="col col-4">
+          <AppCard
+            :title="post.title"
+            :contents="post.contents"
+            :badge="post.badge"
+            :is-like="post.isLike"
+            @toggle-like="post.isLike = !post.isLike"
+          ></AppCard>
+          <button @click="post.isLike = !post.isLike">LIKE</button>
+          <!-- <button @click="likeHandler()">LIKE</button> -->
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import AppCard from "@/components/AppCard.vue";
+import { ref } from "vue";
+
+export default {
+  components: {
+    AppCard,
+  },
+  setup() {
+    const post = ref({
+      title: String,
+      contents: String,
+      badge: String,
+      isLike: Boolean,
+    });
+
+    const posts = ref([
+      {
+        id: 1,
+        title: "TITLE1",
+        contents: "CONTENTS1",
+        badge: "NEWS",
+        isLike: true,
+      },
+      {
+        id: 2,
+        title: "TITLE2",
+        contents: "CONTENTS2",
+        badge: "NEWS",
+        isLike: false,
+      },
+      {
+        id: 3,
+        title: "TITLE3",
+        contents: "CONTENTS3",
+        badge: "NOTICE",
+        isLike: true,
+      },
+      {
+        id: 4,
+        title: "TITLE4",
+        contents: "CONTENTS4",
+        badge: "NEWS",
+        isLike: true,
+      },
+      {
+        id: 5,
+        title: "TITLE5",
+        contents: "CONTENTS5",
+        badge: "NEWS",
+        isLike: false,
+      },
+      {
+        id: 6,
+        title: "TITLE6",
+        contents: "CONTENTS6",
+        badge: "NOTICE",
+        isLike: true,
+      },
+    ]);
+    const likeHandler = () => {
+      console.log(post);
+    };
+    return { post, posts, likeHandler };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
